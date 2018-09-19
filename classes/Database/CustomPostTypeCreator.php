f418a880ed83778a1fb3b75d92b73d14ad70775a
Josh Crawmer <joshcrawmer4@yahoo.com>
2018-09-19 17:33:56 -0500

    changes

--- classes/Database/CustomPostTypeCreator.php
+++ classes/Database/CustomPostTypeCreator.php
@@ -540,14 +540,11 @@ class CustomPostTypeCreator
             'show_in_admin_bar'   => true,
             'menu_position'       => 5,
             'can_export'          => true,
-            'has_archive'         => true,
+            'has_archive'         => false,
             'exclude_from_search' => false,
             'publicly_queryable'  => true,
             'capability_type'     => array('cap_scholarship','cap_scholarships'),
             'map_meta_cap'        => true,
-            /*'capabilities' => array(
-                'create_posts' => 'do_not_allow', // false < WP 4.5, credit @Ewout
-            ),*/
         );
 
         // Registering your Custom Post Type
